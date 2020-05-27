6810a76bc1e5fd93424862db1758aff015cae6b0
lucasandrade52 <lucasandrade_52@hotmail.com>
2020-05-27 14:55:16 -0300

    alterado js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let priceTwo = document.querySelector(".container__card--two .price");
 let priceThree = document.querySelector(".container__card--three .price");
 let switchInput = document.querySelector("#switch");
 
-let antigoValorUm = "19.99"
-let antigoValorDois = "24.99"
-let antigoValorTres = "34.99"
+let antigoValorUm = "199.99"
+let antigoValorDois = "249.99"
+let antigoValorTres = "399.99"
 
-let novoValorUm = "$29.99"
-let novoValorDois = "$35.99"
-let novoValorTres = "$35.99"
+let novoValorUm = "19.99"
+let novoValorDois = "24.99"
+let novoValorTres = "39.99"
 
 switchInput.addEventListener("change", function(){
   
@@ -28,3 +28,39 @@ switchInput.addEventListener("change", function(){
   }  
   
 });
+
+// modal
+const modal = document.getElementById("modal__professional");
+const voltar = document.querySelector(".voltar")
+function onoff() {
+  
+  modal.classList.toggle('hide');
+
+}
+
+  const button = document.querySelector(".container__card--two button");
+  button.addEventListener('click', onoff)
+  voltar.addEventListener('click', onoff)
+
+//
+
+const basic = {
+  title: "Basic", 
+  storage: "500gb",
+  users: "2 users",
+  send: "3GB",
+}
+
+const professional = {
+  title: "Professional", 
+  storage: "1TB",
+  users: "5 users",
+  send: "10GB",
+}
+
+const master = {
+  title: "master", 
+  storage: "2TB",
+  users: "10 users",
+  send: "20GB",
+}
